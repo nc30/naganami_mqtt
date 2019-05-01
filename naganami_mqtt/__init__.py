fb22c7877cb30df30d41527b3c20397a0779d942
Shoji Imamura <imamura@grimoire.co>
2019-05-01 13:01:53 +0900

    refact iot job

--- naganami_mqtt/__init__.py
+++ naganami_mqtt/__init__.py
@@ -1,3 +1,7 @@
+from logging import getLogger, NullHandler
+libLogger = getLogger('naganami_mqtt')
+libLogger.addHandler(NullHandler())
+
 from .mqtt_base import MqttController
 from . import awsiot
 
